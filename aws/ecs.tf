27232c35f0b1a101f1ce1e8777b4766d0238dd73
waynet <wei.tong.nuim@gmail.com>
2023-10-20 10:49:46 +0100

    Add app service resource, fix the task definition arn.

--- aws/ecs.tf
+++ aws/ecs.tf
@@ -28,7 +28,7 @@ resource "aws_iam_role" "ecs_app_execution_role" {
 # Attach the AWS provided policy to the ecs_app_execution_role
 resource "aws_iam_policy_attachment" "ecs_app_execution_role_attachment" {
   name = "ecs_app_execution_role_attachment"
-  roles = [aws_iam_role.ecs_app_execution_role]
+  roles = [aws_iam_role.ecs_app_execution_role.name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
@@ -38,10 +38,27 @@ resource "aws_ecs_task_definition" "app_task_definiation" {
   family = "app-task"
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  execution_role_arn = aws_iam_role.ecs_app_execution_role
+  execution_role_arn = aws_iam_role.ecs_app_execution_role.arn
 
   container_definitions = jsonencode([{
     name = "app-container"
     image = "nginx:latest"
   }])
+}
+
+# The ECS service running the application
+resource "aws_ecs_service" "app_service" {
+  name = "app-service"
+  cluster = aws_ecs_cluster.app.id
+  task_definition = aws_ecs_task_definition.app_task_definiation.arn
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets = [
+      aws_subnet.public_1.id,
+      aws_subnet.public_2.id,
+      aws_subnet.public_3.id
+    ]
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
 }
