b57b13143f9120ebdbbc8a99a23683f149fd6a21
waynet <wei.tong.nuim@gmail.com>
2023-11-04 15:59:00 +0000

    Enable HTTPS for Azure container instance using Caddy sidecar :)

--- azure/container.tf
+++ azure/container.tf
@@ -7,16 +7,52 @@ resource "azurerm_container_group" "app_container_group" {
   resource_group_name = azurerm_resource_group.app_resource_group.name
   os_type             = "Linux"
   dns_name_label = "azure-app-ncirl"
+  # dns_name_label_reuse_policy = "Noreuse"
+  # ip_address_type = "Public"
 
   container {
     name   = "app-container"
-    image  = "nginx:latest"
-    cpu    = "0.5"
-    memory = "0.5"
+    image  = "nginxinc/nginx-unprivileged"
+    cpu    = "1"
+    memory = "1.5"
+    # ports {
+    #   port     = 80
+    #   protocol = "TCP"
+    # }
+  }
+
+  container {
+    name   = "caddy"
+    image  = "caddy"
+    cpu    = "1"
+    memory = "1.5"
+
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
+
     ports {
       port     = 80
       protocol = "TCP"
     }
+
+    volume {
+      name                 = "caddy-data"
+      mount_path           = "/data"
+      storage_account_name = azurerm_storage_account.caddy111ppp.name
+      storage_account_key  = azurerm_storage_account.caddy111ppp.primary_access_key
+      share_name           = azurerm_storage_share.caddy.name
+    }
+
+    commands = [
+      "caddy",
+      "reverse-proxy",
+      "--from",
+      "azure-app-ncirl.northeurope.azurecontainer.io",
+      "--to",
+      "localhost:8080"
+    ]
   }
 }
 
@@ -26,4 +62,19 @@ resource "azurerm_public_ip" "my_public_ip" {
   location            = azurerm_resource_group.app_resource_group.location
   resource_group_name = azurerm_resource_group.app_resource_group.name
   allocation_method = "Dynamic"
+}
+
+# Caddy storage for enabling HTTPS
+resource "azurerm_storage_account" "caddy111ppp" {
+  name                      = "caddy111ppp"
+  resource_group_name       = azurerm_resource_group.app_resource_group.name
+  location                  = azurerm_resource_group.app_resource_group.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  enable_https_traffic_only = true
+}
+resource "azurerm_storage_share" "caddy" {
+  name                 = "caddy-data"
+  storage_account_name = azurerm_storage_account.caddy111ppp.name
+  quota = 20
 }
